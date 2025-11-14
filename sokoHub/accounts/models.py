8ccc11de027d14e77ae6410ec3980e7deefb8109
jimmytana <kubwimanajimmy96@gmail.com>
2025-11-14 11:57:54 +0200

    feat: creating CustomUserModel, Product Model

--- sokoHub/accounts/models.py
+++ sokoHub/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class CustomUser(models.Models):
+    user_type = models.TextField(choices=['vendor', 'customer'])
+    phone = models.TextField()
+    location = models.TextField()
+    def __str__(self):
+        return (f"type: {self.user_type}\n phone: {self.phone}\nlocation: {self.location}")
+    
--- sokoHub/products/models.py
+++ sokoHub/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    vendor = models.IntegerField(models.ForeignKey("accounts.Model", on_delete=models.CASCADE))
+    name = models.TextField()
+    description = models.TextField()
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    image = models.ImageField(_(""), upload_to='/', height_field=None, width_field=None, max_length=None)
+    status = models.TextField()
+    created_at = models.TimeField()
+    
