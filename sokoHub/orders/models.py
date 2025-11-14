f7cc546fda611c4b0d56daf539c56d82af5a3651
gaspard1-sys <gnsengiyumva96@gmail.com>
2025-11-14 02:38:09 -0800

    orders model complete

--- sokoHub/orders/models.py
+++ sokoHub/orders/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class order(models.Model):
-    customer=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    total=models.DecimalFields()
-    status=models.charFields()
-    delivery_adress=models.charFields(max_length=200)
-    phone=models.integerFields()
-    created_at=models.DatetimeField(auto_now_add=True)
+    customer= models.ForeignKey("accounts.Model", on_delete=models.CASCADE)
+    total=models.DecimalField()
+    status=models.CharField()
+    delivery_adress=models.CharField(max_length=200)
+    phone=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
     def __str__(self):
          return f"Order {self.order_number} — {self.customer.username}"
            
@@ -15,10 +15,10 @@ class order(models.Model):
     
 
 class order_Item(models.Model):
-    order=models.Foreignkey(order,on_delete=models.CASCADE)
-    product=models.ForeinKey(on_delete=models.CASCADE)
-    Quantity=models.DecimalFields()
-    price=models.DecimalFields()
+    order=models.ForeignKey(order,on_delete=models.CASCADE)
+    product=models.ForeignKey(on_delete=models.CASCADE)
+    Quantity=models.DecimalField()
+    price=models.DecimalField()
     def __str__(self):
      return f"{self.product} x {self.quantity}"
 
